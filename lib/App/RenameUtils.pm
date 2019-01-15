d288191fbc95498452755f32f32ba285c7908935
perlancar (on pc-office) <perlancar@gmail.com>
2019-01-15 16:04:15 +0700

    Initial commit

---
+++ lib/App/RenameUtils.pm
@@ -0,0 +1,22 @@
+package App::RenameUtils;
+
+# DATE
+# VERSION
+
+1;
+#ABSTRACT: Utilities related to renaming files
+
+=head1 DESCRIPTION
+
+This distributions provides the following command-line utilities:
+
+# INSERT_EXECS_LIST
+
+
+=head1 SEE ALSO
+
+L<rename> from L<File::Rename>
+
+L<perlmv> from L<App::perlmv>
+
+=cut
